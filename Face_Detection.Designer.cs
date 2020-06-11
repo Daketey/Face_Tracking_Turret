49211ab8804ef5841624bb0d8ad9fc2f6295c7cb
Somya Mahapatra <66672417+Daketey@users.noreply.github.com>
2020-06-11 18:57:34 +0530

    Update Face_Detection.Designer.cs

--- Face_Detection.Designer.cs
+++ Face_Detection.Designer.cs
@@ -1,4 +1,4 @@
-namespace Face_Detection_in_forms
+namespace detect2
 {
     partial class Form1
     {
@@ -29,110 +29,55 @@ namespace Face_Detection_in_forms
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pic = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button = new System.Windows.Forms.Button();
+            this.picbox = new System.Windows.Forms.PictureBox();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.cboDevice = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pic
+            // button
             // 
-            this.pic.Location = new System.Drawing.Point(25, 12);
-            this.pic.Name = "pic";
-            this.pic.Size = new System.Drawing.Size(1060, 470);
-            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pic.TabIndex = 0;
-            this.pic.TabStop = false;
-            this.pic.Click += new System.EventHandler(this.pic_Click);
+            this.button.Location = new System.Drawing.Point(549, 552);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 30);
+            this.button.TabIndex = 0;
+            this.button.Text = "Switch";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
             // 
-            // button1
+            // picbox
             // 
-            this.button1.Location = new System.Drawing.Point(68, 488);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 33);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Detect";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.picbox.Location = new System.Drawing.Point(12, 0);
+            this.picbox.Name = "picbox";
+            this.picbox.Size = new System.Drawing.Size(1159, 546);
+            this.picbox.TabIndex = 1;
+            this.picbox.TabStop = false;
             // 
-            // cboDevice
+            // serialPort1
             // 
-            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboDevice.FormattingEnabled = true;
-            this.cboDevice.Location = new System.Drawing.Point(737, 488);
-            this.cboDevice.Name = "cboDevice";
-            this.cboDevice.Size = new System.Drawing.Size(283, 28);
-            this.cboDevice.TabIndex = 2;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(1111, 138);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
-            this.textBox1.TabIndex = 3;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(1111, 236);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 26);
-            this.textBox2.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1134, 115);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 20);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "X_Pos";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1139, 213);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 20);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Y_Pos";
+            this.serialPort1.PortName = "COM8";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1249, 610);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.cboDevice);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pic);
+            this.ClientSize = new System.Drawing.Size(1183, 615);
+            this.Controls.Add(this.picbox);
+            this.Controls.Add(this.button);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbox)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pic;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.PictureBox picbox;
         private System.IO.Ports.SerialPort serialPort1;
-        private System.Windows.Forms.ComboBox cboDevice;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
 
